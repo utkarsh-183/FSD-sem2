76fe6a45ba247a572ac699b1a491b2770d6f7ba4
Utkarsh@0011gmail.com <tiwariutkarsh0011@gmail.com>
2026-07-22 11:10:26 +0530

    FSD

---
+++ Day1/Day1.js
@@ -0,0 +1,33 @@
+console.log('hello world')
+
+// variable
+
+var name="Utkarsh";
+
+console.log(name);
+
+console.log(typeof(name));
+
+var name = "amit";
+console.log(name);
+
+//let
+let age = 22;
+console.log(age);
+
+const c = "india";
+console.log(c);
+
+//datatypes
+var name = "tiwari";
+console.log(name);
+console.log(typeof(name));
+
+let s ={
+    name:"amit"
+}
+
+console.log(s);
+
+
+
