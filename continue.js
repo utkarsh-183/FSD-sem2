552df1cb8dc5b7c4e589417ee1f959479e5fc14d
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-17 09:09:25 +0530

    continue 1

--- continue.js
+++ continue.js
@@ -1,6 +1,18 @@
-for(let i=0; i<5; i++){
-    if(i==2) continue;
+// for(let i=0; i<5; i++){
+//     if(i==2) continue;
+//     else{
+//         console.log(i);
+//     }
+// }
+
+let x = 4 , y = 0, z;
+while(x>=0){
+    x--;
+    y++;
+    if(x==y){
+        continue;
+    }
     else{
-        console.log(i);
+        console.log(x,y);
     }
 }
