9d9609c58a2719ebd7062434f86a87aa195f3e1d
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-21 10:53:36 +0530

    Variable and constant

--- variableAndConst.js
+++ variableAndConst.js
@@ -1,10 +1,31 @@
-var name = 'c';
-if(true){
-    var name = 'c++';
-    console.log(name);
-}
-console.log(name);
+// var name = 'c';
+// if(true){
+//     var name = 'c++';
+//     console.log(name);
+// }
+// console.log(name);
 
 
+// const person = {
+//     name : "Utkarsh",
+//     age : 18,
+//     'job Type' : "Student",
+// }
+
+// // assessing using dot operator
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.jobType);
+// console.log("");
+
+// // assessing using array operator
+// console.log(person["name"]);
+// console.log(person["age"]);
+// console.log(person["job Type"]);
+
+let person = {
+    name : "Utkarsh",
+    age : 18,
+};
 
 
