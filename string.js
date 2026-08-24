8149861838bd48b3a532786282c8ca32afd3a88b
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-24 09:39:54 +0530

    Slice in string

--- string.js
+++ string.js
@@ -1,7 +1,14 @@
-let str1 = `Hello`;
-const str = new String(`hello`);
+// let str1 = `Hello`;
+// const str = new String(`hello`);
 
-str1 = str;
+// str1 = str;
 
-console.log(str1 == str);
-console.log(str1 === str);
+// console.log(str1 == str);
+// console.log(str1 === str);
+
+const fruits = ["Apple", "Banana", "Guava"];
+const copy = fruits.slice(0,2);
+console.log(fruits);
+console.log(copy);
+console.log(copy === fruits);
+console.log(copy[0] === fruits[0]);
