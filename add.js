5f57b6b7986ea8eb6dffaf27f6e71526d7b6a1c9
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-24 09:52:23 +0530

    Negative element

--- add.js
+++ add.js
@@ -1,3 +1,8 @@
-const arr = [1,4,5];
-arr.splice(1,0,2,3);  //start from index 1 delete  0 elements add 2 and 3
-console.log(arr); 
+// const arr = [1,4,5];
+// arr.splice(1,0,2,3);  //start from index 1 delete  0 elements add 2 and 3
+// console.log(arr); 
+
+//using negative index
+const arr = [10,20,30,40];
+arr.splice(-1,0,50,60);
+console.log(arr);
