f4230d93c068e382df92dd3b017ee81a59da4e71
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-17 09:44:37 +0530

    Even Number from Array using arrow function without filter

--- EvenNoFromArrow.js
+++ EvenNoFromArrow.js
@@ -1,7 +1,25 @@
-const arr = [10, 15, 20, 25, 30, 35, 40];
+// const arr = [10, 15, 20, 25, 30, 35, 40];
 
-const findEven = (arr) => {
-    return arr.filter((num) => num % 2 === 0);
-};
+// const findEven = (arr) => {
+//     return arr.filter((num) => num % 2 === 0);
+// };
 
-console.log(findEven(arr));
+// console.log(findEven(arr));
+
+//  without filter
+
+let numbers = [10, 15, 20, 25, 30, 35, 40];
+
+function findEven(numbers) {
+    let even = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            even.push(numbers[i]);
+        }
+    }
+
+    return even;
+}
+
+console.log(findEven(numbers));
