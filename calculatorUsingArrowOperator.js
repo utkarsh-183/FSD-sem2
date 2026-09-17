7dabe175e882e75a086df87ee724b9902418cbd6
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-17 09:32:01 +0530

    Calculator using arrow Operator

---
+++ calculatorUsingArrowOperator.js
@@ -0,0 +1,21 @@
+// Calculator using Arrow Functions
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+// Values
+let a = 20;
+let b = 10;
+
+console.log("Addition:", add(a, b));
+
+console.log("Subtraction:", subtract(a, b));
+
+console.log("Multiplication:", multiply(a, b));
+
+console.log("Division:", divide(a, b));
